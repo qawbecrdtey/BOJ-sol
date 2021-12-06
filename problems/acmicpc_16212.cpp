a83959532405ddde2c0e7e650a85f98d720fdbb3
qawbecrdtey <qawbecrdtey@naver.com>
2021-12-06 12:41:10 +0900

    Added problem number 16212.

---
+++ problems/acmicpc_16212.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int n; cin >> n;
+	auto a = new int[n];
+	for(int i = 0; i < n; i++) cin >> a[i];
+	sort(a, a + n);
+	for(int i = 0; i < n; i++) cout << a[i] << ' ';
+}
