565ddfad87cbd9dc9c0d30478a8d3de9576e28ef
qawbecrdtey <qawbecrdtey@naver.com>
2022-06-02 14:09:40 +0900

    Modified problem number 23235.

--- problems/acmicpc_23235.cpp
+++ problems/acmicpc_23235.cpp
@@ -1,2 +1,9 @@
 #include <cstdio>
-int main(){int n,t=0;while(scanf("%d",&n)){if(!n)break;printf("Case %d: Sorting... done!\n",++t);while(n--)scanf("%*d");}}
+int main() {
+    int n, t = 0;
+    while (scanf("%d", &n)) {
+        if (!n)break;
+        printf("Case %d: Sorting... done!\n", ++t);
+        while (n--)scanf("%*d");
+    }
+}
