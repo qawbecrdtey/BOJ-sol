2574a855cdf0d41f9f916836407a93ceb6ddc4e9
qawbecrdtey <qawbecrdtey@naver.com>
2022-08-23 13:40:43 +0900

    Added problem number 6193.

--- doc/README.md
+++ doc/README.md
@@ -762,6 +762,8 @@
 
 [6186 Best Grass](../problems/acmicpc_6186.cpp) ([링크](https://www.acmicpc.net/problem/6186))
 
+[6193 Hungry Cows](../problems/acmicpc_6193.cpp) ([링크](https://www.acmicpc.net/problem/6193))
+
 [6198 옥상 정원 꾸미기](../problems/acmicpc_6198.cpp) ([링크](https://www.acmicpc.net/problem/6198))
 
 [6219 소수의 자격](../problems/acmicpc_6219.cpp) ([링크](https://www.acmicpc.net/problem/6219))
---
+++ problems/acmicpc_6193.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	int n; cin >> n;
+	vector<int> v;
+	for(int i = 0; i < n; i++) {
+		int x; cin >> x;
+		if(v.empty() || v.back() < x) {
+			v.push_back(x);
+			continue;
+		}
+		*lower_bound(v.begin(), v.end(), x) = x;
+	} cout << v.size();
+}
