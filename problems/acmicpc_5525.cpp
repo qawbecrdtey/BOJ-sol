103edd145123b3ff7748dd15c2dbfe388c91a00b
qawbecrdtey <qawbecrdtey@naver.com>
2024-03-02 12:34:57 +0900

    Modified problem 5525.

--- problems/acmicpc_5525.cpp
+++ problems/acmicpc_5525.cpp
@@ -3,35 +3,17 @@
 using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int n, l;
-    string s;
-    cin >> n >> l >> s;
-    int cnt = 0;
+    cin.tie(nullptr); cout.tie(nullptr);
+    int n, m; string s; cin >> n >> m >> s;
     int res = 0;
-    for(int i = 0; i < l - 1;) {
-        if(cnt < n) {
-            if(s[i] == 'I' && s[i + 1] == 'O') {
-                cnt++; i += 2;
-                continue;
-            }
-            cnt = 0;
-            i++;
-            continue;
-        }
-        if(s[i] == 'I') {
-            res++;
-            if(s[i + 1] == 'O') {
-                i += 2;
-                continue;
-            }
-            cnt = 0;
-            i++;
-            continue;
-        }
-        cnt = 0;
-        i++;
+    int idx = 0;
+    while(idx < m) {
+        while(idx < m && s[idx] == 'O') idx++;
+        if(idx == m) break;
+        int cnt = 0;
+        while(idx < m - 2 && s[idx + 1] == 'O' && s[idx + 2] == 'I') idx += 2, cnt++;
+        res += max(0, cnt - n + 1);
+        idx++;
     }
-    printf("%d", res);
+    cout << res;
 }
